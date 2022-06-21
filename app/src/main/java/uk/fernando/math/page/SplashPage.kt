12c6079c3f671d3135fec18e1a7ffae65dab6f0b
fernandocristiano <75613223+fernandocristiano@users.noreply.github.com>
2022-06-21 08:47:50 +0100

    FR: Splash Screen add title

--- app/src/main/java/uk/fernando/math/page/SplashPage.kt
+++ app/src/main/java/uk/fernando/math/page/SplashPage.kt
@@ -2,9 +2,9 @@ package uk.fernando.math.page
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -13,6 +13,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
@@ -28,14 +30,26 @@ fun SplashPage(
 ) {
     Box(Modifier.fillMaxSize()) {
 
-        Image(
-            painter = painterResource(id = R.mipmap.ic_launcher),
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxSize(0.4f)
+        Column(
+            Modifier
                 .offset(y = (-50).dp)
-                .align(Alignment.Center)
-        )
+                .fillMaxWidth(0.8f)
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id = R.mipmap.ic_launcher),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(0.6f).aspectRatio(1f)
+            )
+
+            Text(
+                text = stringResource(R.string.splash_title),
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 
     val isDarkMode = isSystemInDarkTheme()
